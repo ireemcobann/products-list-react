8d803e5972c455a4e5a8e0f49277bedf5ca064fa
İrem Çoban <iremcoban@192.168.1.117>
2025-01-05 19:03:08 +0300

    güncelleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,75 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
-  return (
-
-    <Productlist />
-
-  )
+  return <Productlist />;
 }
 
-export default App
-
-
+export default App;
 
 function Productlist() {
-
-  const [count, setCount] = useState(0);
-  function HandleClick() {
-    setCount(count)
-    console.log('tiktik')
-  }
+  const [cartItems, setCartItems] = useState([]); 
 
   const products = [
     {
       name: 'Waffle with Berries',
       category: 'Waffle',
-      price: 6.50,
-      image: "/images/waffle.svg"
+      price: 6.5,
+      image: '/images/waffle.svg',
     },
     {
       name: 'Vanilla Bean Crème Brûlée',
       category: 'Crème Brûlée',
-      price: 7.00,
-      image: "/images/cremeburulee.svg"
+      price: 7.0,
+      image: '/images/cremeburulee.svg',
     },
     {
       name: 'Macaron Mix of Five',
       category: 'Macaron',
-      price: 8.00,
-      image: "/images/macaron.svg"
+      price: 8.0,
+      image: '/images/macaron.svg',
     },
     {
       name: 'Classic Tiramisu',
       category: 'Tiramisu',
-      price: 5.50,
-      image: "/images/tiramisu.svg"
+      price: 5.5,
+      image: '/images/tiramisu.svg',
     },
     {
       name: 'Pistachio Baklava',
       category: 'Baklava',
-      price: 4.00,
-      image: "/images/baklava.svg"
+      price: 4.0,
+      image: '/images/baklava.svg',
     },
     {
       name: 'Lemon Meringue Pie',
       category: 'Pie',
-      price: 5.00,
-      image: "/images/pie.svg"
+      price: 5.0,
+      image: '/images/pie.svg',
     },
     {
       name: 'Red Velvet Cake',
       category: 'Cake',
-      price: 4.50,
-      image: "/images/cake.svg"
+      price: 4.5,
+      image: '/images/cake.svg',
     },
     {
       name: 'Salted Caramel Brownie',
       category: 'Brownie',
-      price: 4.50,
-      image: "/images/brownie.svg"
+      price: 4.5,
+      image: '/images/brownie.svg',
     },
     {
       name: 'Vanilla Panna Cotta',
       category: 'Panna Cotta',
-      price: 6.50,
-      image: "/images/pannacotta.svg"
+      price: 6.5,
+      image: '/images/pannacotta.svg',
     },
   ];
 
+  function handleAddToCart(product) {
+    setCartItems([...cartItems, product]);
+}
 
   return (
     <>
@@ -90,8 +81,8 @@ function Productlist() {
               <div className="products-grid-item">
                 <div className="image-container">
                   <img src={product.image} alt={product.name} />
-                  <button onClick={HandleClick}
-                    data-irem={product.name}
+                  <button
+                    onClick={() => handleAddToCart(product)}
                     className="sepete-ekle"
                   >
                     <i className="fa-solid fa-cart-plus"></i>Add to cart
@@ -105,16 +96,36 @@ function Productlist() {
           ))}
         </div>
         <div className="orders-container">
-          <h4 className="cart-title">Your Cart(0)</h4>
+          <h4 className="cart-title">Your Cart({cartItems.length})</h4>
           <div className="orders-grid-item">
-            <img className="sepeteekle" src="assets/images/sepeteekle.svg" alt="" />
-            <span>Your added items will appear here</span>
+            {cartItems.length === 0 ? (
+              <>
+                <img
+                  className="sepeteekle"
+                  src="assets/images/sepeteekle.svg"
+                  alt=""
+                />
+                <span>Your added items will appear here</span>
+              </>
+            ) : (
+              cartItems.map((item, index) => (
+                <div key={index} className="cart-item">
+                  <img src={item.image} className="cart-image" style={{width: '100px'}} />
+                  <div>
+                    <p className="cart-item-name">{item.name}</p>
+                    <p className="cart-item-price">${item.price.toFixed(2)}</p>
+              
+
+                  </div>
+                  
+                </div>
+              ))
+            )}
+                <button className="closeOrderBtn">Start New Order</button>
+
           </div>
         </div>
       </div>
     </>
   );
-  
 }
-
-
